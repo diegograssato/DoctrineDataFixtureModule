26ea931e2f936a413f4d4e33e51a02f698bd9260
Diego Pereira Grassato <dgrassato@ciandt.com>
2017-06-05 18:46:20 -0300

    Add output messsages

--- src/DoctrineDataFixtureModule/Command/ImportCommand.php
+++ src/DoctrineDataFixtureModule/Command/ImportCommand.php
@@ -29,6 +29,7 @@ use Doctrine\DBAL\Migrations\Configuration\Configuration;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use DoctrineDataFixtureModule\Loader\ServiceLocatorAwareLoader;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
@@ -66,17 +67,22 @@ class ImportCommand extends Command
 
         $this->setName('data-fixture:import')
             ->setDescription('Import Data Fixtures')
-            ->setHelp(
-<<<EOT
-The import command Imports data-fixtures
-EOT
-            )
+            ->setHelp('The import command Imports data-fixtures' . PHP_EOL)
             ->addOption('append', null, InputOption::VALUE_NONE, 'Append data to existing data.')
             ->addOption('purge-with-truncate', null, InputOption::VALUE_NONE, 'Truncate tables before inserting data');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $output->writeln(sprintf('<comment>%s</comment>', "Loading fixtures"));
+        $output->writeln(sprintf('<comment>%s</comment>', "----------------"));
+        if ($input->isInteractive() && !$input->getOption('append')) {
+            if (!$this->askConfirmation($input, $output, '<question>Careful, database will be purged. Do you want to continue y/N ?</question>', false)) {
+                return;
+            }
+            $output->writeln("");
+        }
+
         $loader = new ServiceLocatorAwareLoader($this->serviceLocator);
         $purger = new ORMPurger();
 
@@ -85,20 +91,57 @@ EOT
         }
 
         $executor = new ORMExecutor($this->em, $purger);
+        $executor->setLogger(
+            function ($message) use ($output) {
+                $output->writeln(sprintf('  <comment>✔</comment> <info>%s</info>', $message));
+            }
+        );
 
-        foreach ($this->paths as $key => $value) {
-            $loader->loadFromDirectory($value);
+
+        foreach ($this->paths as $value) {
+            if (is_dir($value)) {
+                $loader->loadFromDirectory($value);
+            } elseif (is_file($value)) {
+                $loader->loadFromFile($value);
+            }
         }
-        $executor->execute($loader->getFixtures(), $input->getOption('append'));
+
+        $fixtures = $loader->getFixtures();
+        if (!$fixtures) {
+            throw new \RuntimeException(
+                sprintf('Could not find any fixtures to load in: %s', "\n\n- ".implode("\n- ", $this->paths))
+            );
+        }
+        $executor->execute($fixtures, $input->getOption('append'));
+        $output->writeln("");
     }
 
     public function setPath($paths)
     {
-        $this->paths=$paths;
+        $this->paths = $paths;
     }
 
     public function setEntityManager($em)
     {
         $this->em = $em;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param string          $question
+     * @param bool            $default
+     *
+     * @return bool
+     */
+    private function askConfirmation(InputInterface $input, OutputInterface $output, $question, $default)
+    {
+        if (!class_exists('Symfony\Component\Console\Question\ConfirmationQuestion')) {
+            $dialog = $this->getHelperSet()->get('dialog');
+            return $dialog->askConfirmation($output, $question, $default);
+        }
+        $questionHelper = $this->getHelperSet()->get('question');
+        $question = new ConfirmationQuestion($question, $default);
+        return $questionHelper->ask($input, $output, $question);
     }
 }
